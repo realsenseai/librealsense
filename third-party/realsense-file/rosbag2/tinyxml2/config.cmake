d4bd50ebb4c81f263acf5be0ab43ac0e03d16a9d
Avia Avraham <145359432+AviaAv@users.noreply.github.com>
2025-11-19 10:19:39 +0200

    add foxy packages for rosbag2, dummy reader/writer

---
+++ third-party/realsense-file/rosbag2/tinyxml2/config.cmake
@@ -0,0 +1,10 @@
+cmake_minimum_required(VERSION 3.10)
+
+set(HEADER_FILES_TINYXML2
+    ${CMAKE_CURRENT_LIST_DIR}/tinyxml2.h
+)
+
+set(SOURCE_FILES_TINYXML2
+    ${CMAKE_CURRENT_LIST_DIR}/tinyxml2.cpp
+)
+
