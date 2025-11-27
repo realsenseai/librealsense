5443e6947f8a7ebd38fdd00994691abd13989a36
Avia Avraham <145359432+AviaAv@users.noreply.github.com>
2025-11-27 16:37:59 +0200

    rework sqlite3 inclusion

--- third-party/realsense-file/rosbag2/config.cmake
+++ third-party/realsense-file/rosbag2/config.cmake
@@ -1,14 +1,38 @@
-cmake_minimum_required(VERSION 3.10)
+if(NOT BUILD_ROSBAG2)
+    message(STATUS "rosbag2: BUILD_ROSBAG2=OFF - skipping build of rosbag2 third-party components")
+    return()
+endif()
 
-# include(${CMAKE_CURRENT_LIST_DIR}/console_bridge/config.cmake)
-# include(${CMAKE_CURRENT_LIST_DIR}/ament_index_cpp/config.cmake)
-# include(${CMAKE_CURRENT_LIST_DIR}/class_loader/config.cmake)
-# include(${CMAKE_CURRENT_LIST_DIR}/rcpputils/config.cmake)
-# include(${CMAKE_CURRENT_LIST_DIR}/rcutils/config.cmake)
-# include(${CMAKE_CURRENT_LIST_DIR}/pluginlib/config.cmake)
-# include(${CMAKE_CURRENT_LIST_DIR}/rosbag2_storage/config.cmake)
-# include(${CMAKE_CURRENT_LIST_DIR}/rosbag2_storage_default_plugins/config.cmake)
-# include(${CMAKE_CURRENT_LIST_DIR}/tinyxml2/config.cmake)
+
+#set(CMAKE_CXX_STANDARD 14)
+
+# -- zstd --
+file(GLOB_RECURSE ZSTD_SOURCES
+    "${CMAKE_CURRENT_LIST_DIR}/zstd/zstd.c"
+)
+
+file(GLOB_RECURSE ZSTD_HEADERS
+    "${CMAKE_CURRENT_LIST_DIR}/zstd/*.h"
+)
+
+
+set(CLASS_LOADER_IGNORE_AMENT TRUE CACHE BOOL "Do not use ament" FORCE) # for class_loader
+
+include(${CMAKE_CURRENT_LIST_DIR}/tinyxml2/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/rcutils/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/pluginlib/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/rcpputils/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/rosbag2_storage/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/rosbag2_storage_default_plugins/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/rosbag2_cpp/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/rosbag2_compression/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/console_bridge/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/class_loader/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/ament_index_cpp/config.cmake)
+# Temporary workaround as we reach here twice for some reason...
+if(NOT TARGET SQLite3)
+    include(${CMAKE_CURRENT_LIST_DIR}/sqlite3/config.cmake)
+endif()
 
 set(HEADER_FILES_ROSBAG2
     ${HEADER_FILES_CONSOLE_BRIDGE_ROS2}
@@ -20,6 +44,7 @@ set(HEADER_FILES_ROSBAG2
     ${HEADER_FILES_ROSBAG2_STORAGE}
     ${HEADER_FILES_ROSBAG2_STORAGE_DEFAULT_PLUGINS}
     ${HEADER_FILES_TINYXML2}
+    ${HEADER_FILES_SQLITE3}
     ${ZSTD_HEADERS}
 )
 
@@ -33,6 +58,7 @@ set(SOURCE_FILES_ROSBAG2
     ${SOURCE_FILES_ROSBAG2_STORAGE}
     ${SOURCE_FILES_ROSBAG2_STORAGE_DEFAULT_PLUGINS}
     ${SOURCE_FILES_TINYXML2}
+    ${SOURCE_FILES_SQLITE3}
     ${ZSTD_SOURCES}
 )
 
@@ -46,9 +72,14 @@ set(ROSBAG2_HEADER_DIRS
     ${CMAKE_CURRENT_LIST_DIR}/rosbag2_storage/include/
     ${CMAKE_CURRENT_LIST_DIR}/rosbag2_storage_default_plugins/include/
     ${CMAKE_CURRENT_LIST_DIR}/tinyxml2/
+    ${HEADER_DIR_SQLITE3}
 )
 
-set(SOURCE_FILES_ROSBAG2 ${SOURCE_FILES_ROSBAG2} PARENT_SCOPE)
-
-set(HEADER_FILES_ROSBAG2 ${HEADER_FILES_ROSBAG2} PARENT_SCOPE)
-set(ROSBAG2_HEADER_DIRS ${ROSBAG2_HEADER_DIRS} PARENT_SCOPE)
+# set(SOURCE_FILES_ROSBAG2 ${SOURCE_FILES_ROSBAG2} PARENT_SCOPE)
+# 
+# set(HEADER_FILES_ROSBAG2 ${HEADER_FILES_ROSBAG2} PARENT_SCOPE)
+# set(ROSBAG2_HEADER_DIRS ${ROSBAG2_HEADER_DIRS} PARENT_SCOPE)
+# 
+# 
+# message(STATUS "ROSBAG2_COMPILE_FLAGS (INTERNAL): ${ROSBAG2_COMPILE_FLAGS}")
+# set(ROSBAG2_COMPILE_FLAGS "${ROSBAG2_COMPILE_FLAGS}" PARENT_SCOPE) # PARENT_SCOPE to propagate to parent cmake files
