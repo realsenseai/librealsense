ca0a3ba00f49df2310811d38cca37f72d87b88d6
Avia Avraham <145359432+AviaAv@users.noreply.github.com>
2025-11-30 15:46:58 +0200

    replace main CMakeLists.txt with config.cmake

--- third-party/realsense-file/config.cmake
+++ third-party/realsense-file/config.cmake
@@ -3,11 +3,7 @@ set(LZ4_DIR ${CMAKE_CURRENT_LIST_DIR}/lz4)
 
 set(LZ4_INCLUDE_PATH ${LZ4_DIR}/lib/)
 include(${ROSBAG_DIR}/config.cmake)
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/rosbag2
-                 ${CMAKE_CURRENT_BINARY_DIR}/rosbag2_build)
-# Include rosbag2 configuration
-#include(${ROSBAG_DIR}2/config.cmake)
-
+include(${ROSBAG_DIR}2/config.cmake)
 
 # ---- FetchContent for direct library dependencies ----
 include(FetchContent)
