1d124cc6efe60539b9153742fea5d398392f2dcb
Avia Avraham <145359432+AviaAv@users.noreply.github.com>
2025-11-30 17:31:25 +0200

    remove SHA check from sqlite3

--- third-party/realsense-file/rosbag2/sqlite3/config.cmake
+++ third-party/realsense-file/rosbag2/sqlite3/config.cmake
@@ -1,12 +1,8 @@
 set(SQLITE3_VERSION "3.49.1")
 set(SQLITE3_DOWNLOAD_URL "https://sqlite.org/2025/sqlite-amalgamation-3490100.zip")
-set(SQLITE3_SHA3_256 "e7eb4cfb2d95626e782cfa748f534c74482f2c3c93f13ee828b9187ce05b2da7")
 
 include(FetchContent)
-FetchContent_Declare(sqlite3_ext 
-            URL ${SQLITE3_DOWNLOAD_URL} 
-            URL_HASH SHA3_256=${SQLITE3_SHA3_256}
-            DOWNLOAD_EXTRACT_TIMESTAMP TRUE )
+FetchContent_Declare(sqlite3_ext URL ${SQLITE3_DOWNLOAD_URL})
 FetchContent_MakeAvailable(sqlite3_ext)
 
 # Just set the variable with the main sqlite3 source and header files
