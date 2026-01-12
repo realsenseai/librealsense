72559bdc5b65d994a0827acacc56f121839f9745
Johnny <johnnynuca14@gmail.com>
2026-01-12 19:35:34 +0100

    Update CMake minimum version and CUDA architecture settings
    Updated minimum CMake version and added CUDA architecture support based on CUDA version.

--- CMake/cuda_config.cmake
+++ CMake/cuda_config.cmake
@@ -12,4 +12,40 @@ message(STATUS "CUDA_LIBRARIES: ${CUDA_INCLUDE_DIRS} ${ALL_CUDA_LIBS}")
 
 set(CUDA_PROPAGATE_HOST_FLAGS OFF)
 set(CUDA_SEPARABLE_COMPILATION ON)
-set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS}; -O3 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_110,code=sm_110 -gencode arch=compute_120,code=sm_120;")
+
+# Build CUDA architecture list based on CUDA version
+if(CUDA_VERSION VERSION_LESS "13.0")
+    set(CUDA_ARCH_LIST 62)  # Pascal (CUDA 8.0-12.x, dropped in CUDA 13.0)
+else()
+    set(CUDA_ARCH_LIST)
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "10.0")
+    list(APPEND CUDA_ARCH_LIST 75)  # Turing
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "11.0")
+    list(APPEND CUDA_ARCH_LIST 80 86)  # Ampere
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "11.8")
+    list(APPEND CUDA_ARCH_LIST 89)  # Ada Lovelace
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "12.0")
+    list(APPEND CUDA_ARCH_LIST 90)  # Hopper
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "12.8")
+    list(APPEND CUDA_ARCH_LIST 100 120)  # B200/RTX 50/DGX Spark
+endif()
+if(CUDA_VERSION VERSION_GREATER_EQUAL "13.0")
+    list(APPEND CUDA_ARCH_LIST 110)  # Jetson Thor
+endif()
+
+# Check if variable is available (means CMake >= 3.18)
+if(POLICY CMP0104)
+    # Use modern approach
+    cmake_policy(SET CMP0104 NEW)
+    set(CMAKE_CUDA_ARCHITECTURES ${CUDA_ARCH_LIST})
+else()
+    # Fallback for older CMake: build NVCC flags from architecture list
+    foreach(ARCH ${CUDA_ARCH_LIST})
+        set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -gencode arch=compute_${ARCH},code=sm_${ARCH}")
+    endforeach()
+endif()
