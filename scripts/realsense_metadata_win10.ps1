a1cf9eb2a3e085b053c0bd647116ea7b40d6a716
Wojciech Gladysz <kontra.wojciech.gladysz@gmail.com>
2026-05-13 08:51:19 +0000

    tracked on RSDSO-21412: USB patch for using RealSense VendorID - all cams

--- scripts/realsense_metadata_win10.ps1
+++ scripts/realsense_metadata_win10.ps1
@@ -12,10 +12,10 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-# The scripts is a part of Metadata enablement for IntelRealsense on Windows10 OS.
+# The scripts is a part of Metadata enablement for Realsense on Windows10 OS.
 #
 # The flow is summarized as follows
-# Find all the connected Intel Realsense devices
+# Find all the connected Realsense devices
 # For each device that supports metadata
 #	Check whether the Metadata registry key is present
 #	Add the missing key(s) if necessary
@@ -53,36 +53,36 @@ $SearchTrees = "$DevConfigRegBase\$guid1", "$DevConfigRegBase\$guid2"
 
 # Multipin devices that need additional key MetadataBufferSizeInKB1
 $MultiPinDevices =
-    "USB\VID_8086&PID_0AD1&MI_00",# D400(PSR)
-    "USB\VID_8086&PID_0AD2&MI_00",# D410(ASR)
-    "USB\VID_8086&PID_0AD3&MI_00",# D410+RGB(ASRC)
-    "USB\VID_8086&PID_0AD4&MI_00",# D430(AWG)
-    "USB\VID_8086&PID_0AD5&MI_00",# D430+MM(AWGT)
-    "USB\VID_8086&PID_0AD6&MI_00",# D400/ USB2
-    "USB\VID_8086&PID_0AF6&MI_00",# D420 (PWG)
-    "USB\VID_8086&PID_0AFE&MI_00",# D420+MM(PWGT)
-    "USB\VID_8086&PID_0AFF&MI_00",# D410+MM(ASRT)
-    "USB\VID_8086&PID_0B00&MI_00",# D400+MM(PSRT)
-    "USB\VID_8086&PID_0B01&MI_00",# D430+RGB+MM(AWGCT)
-    "USB\VID_8086&PID_0B03&MI_00",# D460(DS5U)
-    "USB\VID_8086&PID_0B07&MI_00",# D430+RGB(AWGC)
-    "USB\VID_8086&PID_0B0C&MI_00",# D405
-    "USB\VID_8086&PID_0B0D&MI_00",# L500
-    "USB\VID_8086&PID_0B3A&MI_00",# D435i
-    "USB\VID_8086&PID_0B3D&MI_00",# L515 PRQ
-    "USB\VID_8086&PID_0B49&MI_00",# F416
-    "USB\VID_8086&PID_0B4B&MI_00",# D430i
-    "USB\VID_8086&PID_0B4D&MI_00",# D465
-    "USB\VID_8086&PID_0B52&MI_00",# F416+RGB
-    "USB\VID_8086&PID_0B5B&MI_00",# D405
-    "USB\VID_8086&PID_0B5C&MI_00",# D455
-    "USB\VID_8086&PID_0B64&MI_00",# L515
-    "USB\VID_8086&PID_0B68&MI_00",# L535
-    "USB\VID_8086&PID_0B56&MI_00",# D555
-    "USB\VID_8086&PID_1155&MI_00",# D421
-    "USB\VID_8086&PID_1156&MI_00", # D436
-    "USB\VID_8086&PID_0B6A&MI_00",# D585
-    "USB\VID_8086&PID_0B6B&MI_00" # D585S
+    "USB\VID_38E5&PID_0AD1&MI_00",# D400(PSR)
+    "USB\VID_38E5&PID_0AD2&MI_00",# D410(ASR)
+    "USB\VID_38E5&PID_0AD3&MI_00",# D410+RGB(ASRC)
+    "USB\VID_38E5&PID_0AD4&MI_00",# D430(AWG)
+    "USB\VID_38E5&PID_0AD5&MI_00",# D430+MM(AWGT)
+    "USB\VID_38E5&PID_0AD6&MI_00",# D400/ USB2
+    "USB\VID_38E5&PID_0AF6&MI_00",# D420 (PWG)
+    "USB\VID_38E5&PID_0AFE&MI_00",# D420+MM(PWGT)
+    "USB\VID_38E5&PID_0AFF&MI_00",# D410+MM(ASRT)
+    "USB\VID_38E5&PID_0B00&MI_00",# D400+MM(PSRT)
+    "USB\VID_38E5&PID_0B01&MI_00",# D430+RGB+MM(AWGCT)
+    "USB\VID_38E5&PID_0B03&MI_00",# D460(DS5U)
+    "USB\VID_38E5&PID_0B07&MI_00",# D430+RGB(AWGC)
+    "USB\VID_38E5&PID_0B0C&MI_00",# D405
+    "USB\VID_38E5&PID_0B0D&MI_00",# L500
+    "USB\VID_38E5&PID_0B3A&MI_00",# D435i
+    "USB\VID_38E5&PID_0B3D&MI_00",# L515 PRQ
+    "USB\VID_38E5&PID_0B49&MI_00",# F416
+    "USB\VID_38E5&PID_0B4B&MI_00",# D430i
+    "USB\VID_38E5&PID_0B4D&MI_00",# D465
+    "USB\VID_38E5&PID_0B52&MI_00",# F416+RGB
+    "USB\VID_38E5&PID_0B5B&MI_00",# D405
+    "USB\VID_38E5&PID_0B5C&MI_00",# D455
+    "USB\VID_38E5&PID_0B64&MI_00",# L515
+    "USB\VID_38E5&PID_0B68&MI_00",# L535
+    "USB\VID_38E5&PID_0B56&MI_00",# D555
+    "USB\VID_38E5&PID_1155&MI_00",# D421
+    "USB\VID_38E5&PID_1156&MI_00", # D436
+    "USB\VID_38E5&PID_0B6A&MI_00",# D585
+    "USB\VID_38E5&PID_0B6B&MI_00" # D585S
 
 #Inhibit system warnings and erros, such as permissions or missing values
 $ErrorActionPreference = "silentlycontinue"
@@ -95,24 +95,24 @@ $DevInReg = Get-ChildItem hklm:\SYSTEM\CurrentControlSet\Services\usbvideo | For
 #Transform output into a standard container
 for ($i=0; $i -lt $DevInReg[0].Count; $i++) { $ConnectedDev +=$DevInReg[0].$i}
 
-#Filter Intel Realsense devices
-$ConnectedDev = $ConnectedDev -like "*VID_8086*"
+#Filter Realsense devices
+$ConnectedDev = $ConnectedDev -like "*VID_38E5*"
 
 #Progress notification
 $rs_count = $ConnectedDev.Count
 echo "$rs_count connected RealSense devices were found:" $ConnectedDev
 
-#Search each subtree for values that correspond to the requested Intel Realsense devices
+#Search each subtree for values that correspond to the requested Realsense devices
 foreach ($subtree in $SearchTrees)
 {
     "`nProcessing Registry branch $subtree"
     #Get records for all UVC devices records
     $Items = Get-ChildItem $subtree | Foreach-Object {Get-ItemProperty $_.PsPath }
 
-    #Filter Intel RealSense devices
+    #Filter RealSense devices
     "There are " + $Items.Count +" total devices listed"
-    $Items = $Items | Where { $_.DeviceInstance -like "*VID_8086*" }
-    "" + $Items.Count + " of them are Intel Realsense"
+    $Items = $Items | Where { $_.DeviceInstance -like "*VID_38E5*" }
+    "" + $Items.Count + " of them are Realsense"
 
     $remove_keys = 0
     switch ($op)
@@ -155,14 +155,14 @@ foreach ($subtree in $SearchTrees)
                 "Device " +  $item.DeviceInstance.ToString() + ": skiping - metadata key already exists"
             }
 
-            #convert "USB\VID_8086&PID_0B07&MI_03\6&269496df&0&0003" into "USB\VID_8086&PID_0B07&MI_03"
+            #convert "USB\VID_38E5&PID_0B07&MI_03\6&269496df&0&0003" into "USB\VID_38E5&PID_0B07&MI_03"
             if (($MultiPinDevices -contains $item.DeviceInstance.Substring(0,27)) -and ($val[1] -eq 0))
             {
                 # Multi-pin interface requires an additional key
                 "Device " +  $item.DeviceInstance.ToString() +": adding extra key for multipin interface"
                 Set-ItemProperty -path $fullPath -name MetadataBufferSizeInKB1 -value 5
             }
-			#convert "USB\VID_8086&PID_0B07&MI_03\6&269496df&0&0003" into "USB\VID_8086&PID_0B07&MI_03"
+			#convert "USB\VID_38E5&PID_0B07&MI_03\6&269496df&0&0003" into "USB\VID_38E5&PID_0B07&MI_03"
             if (($MultiPinDevices -contains $item.DeviceInstance.Substring(0,27)) -and ($val[2] -eq 0))
             {
                 # Multi-pin interface requires an additional key
